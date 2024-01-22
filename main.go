363db521fc078211a4765f53fc6a213e68d9e5d0
Fraged <fragedd@gmail.com>
2024-01-22 18:11:35 +0300

    Добавил подсчет оценки за четверть

--- main.go
+++ main.go
@@ -12,27 +12,33 @@ func main() {
 	var name string
 	var function int
 	var i string
-	fmt.Println("What is your name?")
+	fmt.Println("Как тебя зовут?")
 	fmt.Scanln(&name)
-	fmt.Println("Hello", name, "change function: \n 1. Калькулятор \n 2. Нахождение корней квадратного уровнения")
+	fmt.Println("Привет", name, "выбери функцию: \n 1. Калькулятор \n 2. Нахождение корней квадратного уровнения")
+	fmt.Println(" 3. Подсчет оченки за четверть по текущим.")
 	fmt.Scanln(&function)
 	switch {
 	case function == 1:
 		calk()
 	case function == 2:
 		poiskx()
+	case function == 3:
+		ocenka()
 	}
 	fmt.Println("Желаете продолжить? (y/n)")
 	fmt.Scanln(&i)
 
 	for i == "y" {
-		fmt.Println("Hello", name, "change function: \n 1. Калькулятор \n 2. Нахождение корней квадратного уровнения")
+		fmt.Println("Hello", name, "change function: \n 1. Калькулятор. \n 2. Нахождение корней квадратного уровнения.")
+		fmt.Println(" 3. Подсчет оченки за четверть по текущим.")
 		fmt.Scanln(&function)
 		switch {
 		case function == 1:
 			calk()
 		case function == 2:
 			poiskx()
+		case function == 3:
+			ocenka()
 		}
 		fmt.Println("Желаете продолжить? (y/n)")
 		fmt.Scanln(&i)
@@ -99,5 +105,26 @@ func poiskx() {
 	} else if D < 0 {
 		fmt.Println("Ваше уровнение не имеет корней \nD: " + fmt.Sprint(D))
 	}
+	fmt.Scanln()
+}
+
+func ocenka() {
+	var x float64
+	sum := 0.0
+	var srednee float64
+	marks := []float64{}
+	fmt.Println("Введите ваши оценки: (Чтобы прекратитить введите число >5)")
+	for i := 0; x <= 5; i++ {
+		fmt.Scan(&x)
+		marks = append(marks, x)
+		sum = sum + marks[i]
+	}
+	sum = sum - marks[len(marks)-1]
+	marks = marks[:len(marks)-1]
+	kol_vo := float64(len(marks))
+	srednee = math.Round(sum / kol_vo)
+	fmt.Println("Список оценок: \n", marks)
+	fmt.Println("Колличество оценок: ", len(marks))
+	fmt.Println("Оценка которая выходит в четверти: ", srednee)
 	fmt.Scanln()
 }
